ab0e821814e11b3e71a6d72a30b57f807a556fd4
Elena Granados <patty518@yahoo.com>
2019-04-15 07:42:48 -0700

    add components, actions and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// Action Creator
+
+export const SelectSong = song => {
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+};
+
+export default SelectSong;
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const SongsReducer = () => {
+    return [
+        {title: 'No Scrubs' , duration: '4:05'},
+        {title: 'Macarena', duration: '2:30'},
+        {title: 'All Star', duration: '3:15'},
+        {title: 'We Are the World', duration: '1:45'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    
+    return selectedSong;
+
+};
